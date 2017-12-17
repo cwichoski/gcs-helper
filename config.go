a0492bb8489110874fa16ba9265dba29a20332ec
Francisco Souza <francisco.souza@nytimes.com>
2017-12-17 12:29:20 -0500

    config: add options for signing urls
    Loading the private key as base64.
    
    Related to #10.

--- config.go
+++ config.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
+	"net/http"
 	"os"
 	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 )
@@ -20,6 +23,7 @@ type Config struct {
 	MapRegexHDFilter    string   `envconfig:"MAP_REGEX_HD_FILTER"`
 	MapExtraPrefixes    []string `envconfig:"MAP_EXTRA_PREFIXES"`
 	ClientConfig        ClientConfig
+	SignConfig          SignConfig
 }
 
 // ClientConfig contains configuration for the GCS client communication.
@@ -29,6 +33,40 @@ type ClientConfig struct {
 	Timeout         time.Duration `envconfig:"GCS_CLIENT_TIMEOUT" default:"2s"`
 	IdleConnTimeout time.Duration `envconfig:"GCS_CLIENT_IDLE_CONN_TIMEOUT" default:"120s"`
 	MaxIdleConns    int           `envconfig:"GCS_CLIENT_MAX_IDLE_CONNS" default:"10"`
+}
+
+// SignConfig contains configuration for generating signed URLs in mapped mode.
+type SignConfig struct {
+	Expiration time.Duration `envconfig:"GCS_SIGNER_EXPIRATION" default:"20m"`
+	AccessID   string        `envconfig:"GCS_SIGNER_ACCESS_ID"`
+	PrivateKey b64Value      `envconfig:"GCS_SIGNER_PRIVATE_KEY"`
+}
+
+type b64Value []byte
+
+func (v *b64Value) Decode(value string) error {
+	b, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		return err
+	}
+	*v = b
+	return nil
+}
+
+// Options returns the SignedURLOptions that should be used for signing object
+// URLs.
+//
+// When URL signing is disabled, it returns two nil values.
+func (c *SignConfig) Options() (*storage.SignedURLOptions, error) {
+	if c.AccessID == "" || c.PrivateKey == nil {
+		return nil, nil
+	}
+	return &storage.SignedURLOptions{
+		Method:         http.MethodGet,
+		GoogleAccessID: c.AccessID,
+		PrivateKey:     []byte(c.PrivateKey),
+		Expires:        time.Now().Add(c.Expiration),
+	}, nil
 }
 
 func (c Config) logger() *logrus.Logger {
--- config_test.go
+++ config_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"math"
+	"net/http"
 	"os"
 	"reflect"
 	"testing"
 	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,6 +24,9 @@ func TestLoadConfig(t *testing.T) {
 		"GCS_CLIENT_TIMEOUT":             "60s",
 		"GCS_CLIENT_IDLE_CONN_TIMEOUT":   "3m",
 		"GCS_CLIENT_MAX_IDLE_CONNS":      "16",
+		"GCS_SIGNER_EXPIRATION":          "10m",
+		"GCS_SIGNER_ACCESS_ID":           "access",
+		"GCS_SIGNER_PRIVATE_KEY":         "c2VjcmV0IQ==",
 	})
 	config, err := loadConfig()
 	if err != nil {
@@ -38,6 +44,11 @@ func TestLoadConfig(t *testing.T) {
 			IdleConnTimeout: 3 * time.Minute,
 			MaxIdleConns:    16,
 			Timeout:         time.Minute,
+		},
+		SignConfig: SignConfig{
+			AccessID:   "access",
+			PrivateKey: []byte("secret!"),
+			Expiration: 10 * time.Minute,
 		},
 	}
 	if !reflect.DeepEqual(config, expectedConfig) {
@@ -59,6 +70,9 @@ func TestLoadConfigDefaultValues(t *testing.T) {
 			IdleConnTimeout: 120 * time.Second,
 			MaxIdleConns:    10,
 			Timeout:         2 * time.Second,
+		},
+		SignConfig: SignConfig{
+			Expiration: 20 * time.Minute,
 		},
 	}
 	if !reflect.DeepEqual(config, expectedConfig) {
@@ -108,9 +122,59 @@ func TestLoadConfigValidation(t *testing.T) {
 	}
 }
 
+func TestSignConfigOptions(t *testing.T) {
+	var tests = []struct {
+		name            string
+		input           SignConfig
+		expectedOptions *storage.SignedURLOptions
+		expectError     bool
+	}{
+		{
+			"valid config",
+			SignConfig{AccessID: testdataAccessKeyID, PrivateKey: []byte(testdataPrivateKey), Expiration: time.Minute},
+			&storage.SignedURLOptions{
+				Method:         http.MethodGet,
+				Expires:        time.Now().Add(time.Minute),
+				GoogleAccessID: "testing@gcs-helper-test.iam.gserviceaccount.com",
+				PrivateKey:     []byte(testdataPrivateKey),
+			},
+			false,
+		},
+		{
+			"no config",
+			SignConfig{Expiration: time.Hour},
+			nil,
+			false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			opts, err := test.input.Options()
+			if test.expectError == (err == nil) {
+				t.Errorf("mismatch in error\nexpectError: %v\ngot error: %#v", test.expectError, err)
+			}
+			if test.expectedOptions != nil {
+				diff := math.Abs(float64(opts.Expires.Sub(test.expectedOptions.Expires)))
+				if time.Duration(diff) > time.Second {
+					t.Errorf("expiration is off by more than one second\nwant: %s\ngot:  %s", test.expectedOptions.Expires, opts.Expires)
+				}
+				opts.Expires = test.expectedOptions.Expires
+			}
+			if !reflect.DeepEqual(opts, test.expectedOptions) {
+				t.Errorf("wrong options returned\nwant %#v\ngot  %#v", test.expectedOptions, opts)
+			}
+		})
+	}
+}
+
 func setEnvs(envs map[string]string) {
 	os.Clearenv()
 	for name, value := range envs {
 		os.Setenv(name, value)
 	}
 }
+
+const (
+	testdataAccessKeyID = "testing@gcs-helper-test.iam.gserviceaccount.com"
+	testdataPrivateKey  = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCkYNVWeHUkggRk\nJrfj1/vcJvZguHIl2h2GXxKHEJ2A0yMLbbPfEyrPA3R8dFXHb3m181H0X6294XJb\nJsJXN0IzXSf6FzBun7gj6tscpQxMO5fOAah5hq6qDpHAU+aj3gKMCBPIOs1nxtrr\nyeDx7zgxU7DzNtqs1BnDytWI/mj3QXUc9wlRQXevOVzzpdxIFIJGLonqRWbzinEq\ncMokVm1y1uXj1RBn5slsATdKShfoHSg1c68qV9EaolJKVhx1ZsnHbGKP90DhXTXT\ntXVkZem0yD2EaXYWtlL7LKUT/Xw/E4vWfASgMynaZI+b9/FDeAmt3ZkixA5ewPED\n+UQfzBT7AgMBAAECggEAIwRBzibhBYLw/ojE+bOEArUGHTqNjoS1b2+HWeBvPQc9\nWuzmuWmy3+CjivOZZl/X9Ku91KohL+b73nEWS1AJOTnqDzurZJV/u58HSEXcpcy4\nHPl7c0/+m1l5MRhudJARyNTbqfbk1OumrT4XPlKwjMmAU39m/BQ+3Nezv3g60hj3\nBXoIAJayWDYSPAgSdwwKI2SNS4re/Qwb16YfCae3ozOt4ZZyGFPp25YiXUXrPR/4\nMtSdTXyOXazam9bj8KRsmfR8W55K8aBnvyiQYkc49LLTDB/G6TAyhaH4+HzeXsQ+\nZLGR93Wr7JZOymLSgKVyjRBLV5v0lCjlbMe8DgGTaQKBgQDcCz0evugR02qM60VT\nxJY8qmVl9RpZwAd4r94i2gVDtE6/t+WUhHh4wJjWdpmqk87gIwRJrZ7b/vkU3qi2\nwzF02D4YSmIk7NwVqAHwQ56g7DWW1ozPh53pXWJxP6bsV31jGRfr15VLlYmuHu1h\nYk4cG0MkPXUoCM9ChYcUoEVfvQKBgQC/PQS4vT6XORRbHYzq42wPYPLYFAtWZzg2\nqc8iTDEfkZ6EusXrlPxnTmwHNSAuMebyoE9ziAVCcssjsyBtkjjnKc1NE08y8284\n3Cs7JFfPhhG/ghOtv2R6mW9Z1UtqfGWQTqMufm7BBFw4vZNWljjjITFvMdvenSMb\nXYwBu/iXFwKBgFLEW3IUJuCFoF9vI32VxVj+UvOd1RKLO4Q2ypxbW32S9cgBWPab\nOWFaOGL662QRAtCl+zfneYiQiIpEEjvkgdbMe9bRK8dt3H682jXQiXtIPgQFoaNy\nBIDB4oRsh9IAOqaqyqeoSHzMu6Pl+C4YNv81dfTMtSOg5KzF4wBsJIwVAoGAT9uE\nKDzmcTGlvXK2kLONQVLDtdWQ8nDB+ZmpZHIapUsivdxcn8akK+OEmvHlUUUHYtPs\nuZrYT2ouR+caKIdB+c3r7D6e+PDMxhqydszzWjZrHOSNoSVmKQf/hqzaBEqUAtHD\ntLuZNkLC2/LWHvc2JCqNQRi57tkBewDyYRsEcNsCgYA9j2Ywv15ef1xBGy0dNqkC\nM39PjCf4XKOjTcyi0UUMQ9LR34WQnYpKULcbz+B9mOTgZ2BjK2tvXdMF+CayjjPo\nMsf4Iru8NbbP/KvpmjIyz6tVjdt4MKs92zueLgQ5IyoAY/jA/mICTnWSjpOOnbtp\n7lRlHMZMLIC0G8es9wujbQ==\n-----END PRIVATE KEY-----\n"
+)
